c75dd63e800bb23b85eeb891b8b742079c490084
Obianuju23 <obainujunmoh@gmail.com>
2023-06-01 15:23:21 -0700

    function that removes a node from a Binary Search Tree

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -57,3 +57,39 @@ bst_t *bst_remove(bst_t *root, int value)
 	}
 	return (root);
 }
+/**
+ * bst_remove - a function that removes a node from  a BST
+ * @root: the root node of the tree to remove from
+ * @value: the value of the node to remove
+ * Return: the new root node of the tree
+ */
+bst_t *bst_remove(bst_t *root, int value)
+{
+	bst_t *tmp, *successor;
+
+	if (root == NULL)
+		return (NULL);
+	if (root->n > value)
+		root->left = bst_remove(root->left, value);
+	else if (root->n < value)
+		root->right = bst_remove(root->right, value);
+	else
+	{
+		if (root->left == NULL)
+		{
+			tmp = root->right;
+			free(root);
+			return (tmp);
+		}
+		else if (root->right == NULL)
+		{
+			tmp = root->left;
+			free(root);
+			return (tmp);
+		}
+		successor = bst_remove_help(root->right);
+		root->n = successor->n;
+		root->right = bst_remove(root->right, successor->n);
+	}
+	return (root);
+}
